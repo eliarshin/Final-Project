5596652548dbc4bf710193043e273982c0ddf519
eliarshin <elikarshin@gmail.com>
2021-11-28 14:59:17 +0200

    learn to use subprocess - get list of mac's addr

---
+++ mac_changer.py
@@ -0,0 +1,6 @@
+import subprocess
+#Information about subprocess library https://docs.python.org/3/library/subprocess.html
+'The subprocess help us to run os comands, line by line not threadable'
+
+output = subprocess.call("getmac", shell=True)
+print(output)
