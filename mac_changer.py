882e70b7a57ca7f2f408bdec9602b846f86da83b
eliarshin <80040465+eliarshin@users.noreply.github.com>
2021-12-03 03:24:47 +0200

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -2,6 +2,8 @@ import subprocess
 #Information about subprocess library https://docs.python.org/3/library/subprocess.html
 'The subprocess help us to run os comands, line by line not threadable'
 
+#Getting the MAC on the computer
 output = subprocess.call("getmac", shell=True)
 print(output)
-print(output)
+
+'Need to be done - Check which command change the MAC address !'
