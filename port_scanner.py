d65f0d241ae73db17f745583c4f62dc02f944b68
eliarshin <80040465+eliarshin@users.noreply.github.com>
2021-12-03 03:44:39 +0200

    port scanner baisc
    #need to imrpove
    #multiple scans
    #thread not working
    #special ports
    #choose type of scan

--- port_scanner.py
+++ port_scanner.py
@@ -1,3 +1,27 @@
-#testing github
-print("test")
-print("hello everyone")
+import threading
+import socket
+
+
+#Target
+target = "google.com"
+
+#port scanner
+def port_scan(port):
+    soc = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # TCP
+    soc.settimeout(0.5)#timeout to establish that connection failed
+    try:
+        connection = soc.connect((target,port))
+        print("Port Open",port)
+        connection.close()
+    except:
+        pass
+
+def main():
+    r = 1
+    for x in range(1,2000):
+        t = threading.Thread(target = port_scan,kwargs={'port':r})
+        r = r + 1
+        t.start()
+if __name__ == "__main__":
+    main()
+
