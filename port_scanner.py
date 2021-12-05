c665c019f27c2181182479101fc71f67acd1396d
eliarshin <elikarshin@gmail.com>
2021-12-05 13:50:23 +0200

    added function that read from json file

--- port_scanner.py
+++ port_scanner.py
@@ -1,7 +1,10 @@
 import socket
+import json
 import sys
 
 class port_scanner:
+    
+    PORTS_TO_SCAN = "./common_ports.json"
 
     #Init our ps struct, including target, which ports are open, and the vulnerabilities
     def __init__(ps):
@@ -9,6 +12,13 @@ class port_scanner:
         ps.open_ports = [] # collecting open ports
         ps.ports_vulnerability = {} # collecting vulnerability information
     
+    #Extracting data from json file to our struct
+    def read_from_json(ps):
+        with open(port_scanner.PORTS_TO_SCAN, "r") as file:
+            data = json.load(file)
+        #print(data)
+        ps.ports_vulnerability = {int(k): v for (k, v) in data.items()}
+
     #Check every port that given with the host, if the connection succeeded, we append it on our struck of open ports
     def port_scan(ps, port):
             #find the best time out to catch all the open ports
