76de6c8bbcba7ad88dc197b98a8c48d448d075e1
eliarshin <elikarshin@gmail.com>
2021-12-08 19:00:38 +0200

    network scanner V1 , tasks to do

--- network_scanner.py
+++ network_scanner.py
@@ -3,10 +3,37 @@
 'Every device send ARP Request to specific MAC address to communicate with him'
 'All devices on same network will ignore the packet, the only device that will respond'
 'is the device that got the IP that sent on ARP , on the response we will get the MAC of the device'
-
+from rich.console import Console
+from rich.table import Table
+from art import *
 import scapy.all as scapy
 
-def scan(ip):
-    scapy.arping(ip)
+#TO DO#
+'Create Constructor'
+'Input from user'
+'Display results'
 
-scan("172.29.96.1")
+def create_packet(ip):
+    arp_req = scapy.ARP(pdst = ip) # create ARP packet object and the current IP 
+    broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")  # create Ethernet packet
+    arp_req_brod = broadcast/arp_req
+    #print(arp_req_brod.summary())
+    #scapy.ls(scapy.ARP()) # show us information of scapy ARP functions.
+    #scapy.arping(ip)
+    return arp_req_brod
+
+def parse(packet):
+    for data in packet:
+        print(data[1].psrc)
+        print(data[1].hwsrc)
+        print("-" * 11)
+
+def send_packet(packet):
+    ans_packet,unans_packet = scapy.srp(packet, timeout = 1)
+    #print(ans_packet.summary()) # clients that used
+    #print(unans_packet.summary()) # no clients using them
+    return ans_packet
+
+ret = create_packet("172.29.96.1/24")
+ans = send_packet(ret)
+parse(ans)
