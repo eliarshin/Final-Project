6cf290b9e2d666864522d0661f75a6003ca98e95
eliarshin <elikarshin@gmail.com>
2022-02-12 20:23:09 +0200

    Subdomains work - directories not yet

--- Web Crawler/web_crawler.py
+++ Web Crawler/web_crawler.py
@@ -1,45 +1,99 @@
 from io import StringIO
+from bs4 import BeautifulSoup
 import requests
 import re
 from urllib.parse import urlparse
 from urllib.parse import urljoin
 
+class web_crawler:
+    def __init__(crawl):
+        crawl.target = ""
+        crawl.found_subdomains = []
+        crawl.subdomain_list = open("./Web Crawler/subdomains_list.txt")
+        crawl.copy_subdomain_list = ""
+        crawl.subdomain_list_content = ""
+        crawl.urls = []
 
-url = "ynet.co.il"
+    def parse_data(crawl):
+        crawl.copy_subdomain_list = crawl.subdomain_list.read()
+        crawl.subdomain_list_content = crawl.copy_subdomain_list.splitlines()
 
-def request(url):
-    try:
-        return requests.get("https://"+url)
-    except requests.exceptions.ConnectionError:
-        pass
-
-def find_subdomains(url):
-    with open("./subdomains_list.list", "r") as wordlist_file:
-        for line in wordlist_file:
-            word = line.strip()
-            test_url = word + "." + url
-            resp = request(test_url)
-            if resp:
-                print("[+] subdomain discovered --> " + test_url)
-
-def find_directory(url):
-    with open("./subdomains_list.list", "r") as wordlist_file:
-        for line in wordlist_file:
-            word = line.strip()
-            test_url =  url + "/" + word
-            resp = request(test_url)
+    def get_target(crawl):
+        crawl.target = input("Enter your target :")
     
-            if resp:
-                print("[+] url discovered --> " + test_url)
+    def find_subdomains(crawl):
+        crawl.parse_data()
+        for subdomain in crawl.subdomain_list_content:
+            url = (f"http://{subdomain}.{crawl.target}")
+            try:
+                requests.get(url)
+            except requests.ConnectionError:
+                pass
+            else:
+                #print(f"Discovered subdomain : :{url}" )
+                crawl.found_subdomains.append(url)
+        for u in crawl.found_subdomains:
+            print("[+]URL - " + u)
+    
+    def find_directories(crawl):
+        get_site = requests.get(crawl.target)
+        soup = BeautifulSoup(get_site.text,"lxml")
 
-def links(url):
-    resp = requests.get("https://"+url)
-    print(resp)
-    reg = b'(?:href=")(.*?)"'
-    href_links = re.findall( reg, resp.content)
-    #href_links.remove("b'")
-    for link in href_links:
-        #print(link)
-        link = urlparse.urljoin(url,link)
-        print(link)
-links(url)
+        for i in soup.find_all("a"):
+            href = i.attrs['href']
+            if href.startswith("/"):
+                crawl.target = crawl.target + href
+                if crawl.target not in crawl.urls:
+                    crawl.urls.append(crawl.target) 
+                    print(crawl.target)
+                    crawl.find_directories()
+
+    
+    def get_started(crawl):
+        crawl.get_target()
+        #crawl.find_subdomains()
+        crawl.find_directories()
+
+
+if __name__ == "__main__":
+    network_scan = web_crawler()
+    network_scan.get_started()
+
+# url = "ynet.co.il"
+
+# def request(url):
+#     try:
+#         return requests.get("https://"+url)
+#     except requests.exceptions.ConnectionError:
+#         pass
+
+# def find_subdomains(url):
+#     with open("./subdomains_list.list", "r") as wordlist_file:
+#         for line in wordlist_file:
+#             word = line.strip()
+#             test_url = word + "." + url
+#             resp = request(test_url)
+#             if resp:
+#                 print("[+] subdomain discovered --> " + test_url)
+
+# def find_directory(url):
+#     with open("./subdomains_list.list", "r") as wordlist_file:
+#         for line in wordlist_file:
+#             word = line.strip()
+#             test_url =  url + "/" + word
+#             resp = request(test_url)
+    
+#             if resp:
+#                 print("[+] url discovered --> " + test_url)
+
+# def links(url):
+#     resp = requests.get("https://"+url)
+#     print(resp)
+#     reg = b'(?:href=")(.*?)"'
+#     href_links = re.findall( reg, resp.content)
+#     #href_links.remove("b'")
+#     for link in href_links:
+#         #print(link)
+#         link = urlparse.urljoin(url,link)
+#         print(link)
+# links(url)
