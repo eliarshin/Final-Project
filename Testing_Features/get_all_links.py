c326ad2260b008d90ea8fb60b3297bc17d39d566
eliarshin <elikarshin@gmail.com>
2022-05-11 22:51:00 +0300

    deploying all my tests

---
+++ Testing_Features/get_all_links.py
@@ -0,0 +1,10 @@
+from io import BytesIO
+from lxml import etree
+import requests
+url = "http://jce.ac.il"
+r = requests.get(url) # GET
+content = r.content # content is of type 'bytes'
+parser = etree.HTMLParser()
+content = etree.parse(BytesIO(content), parser=parser) # Parse into tree
+for link in content.findall('//a'): # find all "a" anchor elements.
+    print(f"{link.get('href')} -> {link.text}")
